9a94aa8e0773e3221590b2c9d94d5bb40dec8381
sajid-munawar <sajidmunawar666@gmail.com>
2023-05-29 17:16:10 +0500

    Update styles

--- app/sections/MobilePopUp.tsx
+++ app/sections/MobilePopUp.tsx
@@ -13,7 +13,7 @@ export default function MobilePopUp() {
       {/* Mobile popup open and close svgs */}
       <div
         onClick={handleOpen}
-        className="fixed right-28 top-16 z-10 lg:hidden"
+        className="fixed right-28 top-16 z-20 lg:hidden"
       >
         {open ? (
           <>
@@ -59,7 +59,7 @@ export default function MobilePopUp() {
         )}
       </div>
       {/* Dine market logo */}
-      <div className="fixed left-28 top-16 z-10 lg:hidden">
+      <div className="fixed left-28 top-16 z-20 lg:hidden">
         <Image
           src="/Logo.webp"
           height={25}
@@ -68,44 +68,42 @@ export default function MobilePopUp() {
         />
       </div>
       {!open && (
-        <div className="lg:hidden flex justify-center items-center h-screen w-full bg-white">
+        <div className="lg:hidden flex justify-center items-center flex-col w-full bg-white gap-4 fixed top-0 left-0 bottom-0 right-0 z-10">
           {/* List of links */}
-          <div className="flex flex-col items-center gap-4 fixed">
-            {/* Cart button */}
-            <button className="bg-gray-200 rounded-full p-3 relative">
-              <svg
-                stroke="currentColor"
-                fill="none"
-                stroke-width="0"
-                viewBox="0 0 24 24"
-                height="22"
-                width="22"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  clip-rule="evenodd"
-                  d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
-                  fill="currentColor"
-                ></path>
-                <path
-                  d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
-                  fill="currentColor"
-                ></path>
-                <path
-                  d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
-                  fill="currentColor"
-                ></path>
-              </svg>
-              <span className="bg-red-600 text-white rounded-full h-4 w-4 text-sm absolute top-0 right-1">
-                0
-              </span>
-            </button>
-            <div>Female</div>
-            <div>Male</div>
-            <div>Kids</div>
-            <div>All Products</div>
-          </div>
+          {/* Cart button */}
+          <button className="bg-gray-200 rounded-full p-3 relative">
+            <svg
+              stroke="currentColor"
+              fill="none"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              height="22"
+              width="22"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
+                fill="currentColor"
+              ></path>
+              <path
+                d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
+                fill="currentColor"
+              ></path>
+              <path
+                d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
+                fill="currentColor"
+              ></path>
+            </svg>
+            <span className="bg-red-600 text-white rounded-full h-4 w-4 text-sm absolute top-0 right-1">
+              0
+            </span>
+          </button>
+          <div>Female</div>
+          <div>Male</div>
+          <div>Kids</div>
+          <div>All Products</div>
         </div>
       )}
     </>
