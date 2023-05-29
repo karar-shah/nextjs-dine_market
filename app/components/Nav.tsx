618b5e0dad7a642623e18aeb0435efa0d437923f
sajid-munawar <sajidmunawar666@gmail.com>
2023-05-29 15:13:11 +0500

    Update navbar

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,83 +1,113 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 
-export default function Nav() {
+export default function MobilePopUp() {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
   return (
     <>
-      <nav className="flex justify-between my-8 items-center font-medium tracking-wide">
-        <div>
-          <a href="/">
-            <Image src={"/Logo.webp"} alt="Dine Logo" width={140} height={25} />
-          </a>
-        </div>
-        <div className="flex gap-12">
-          <div>Female</div>
-          <div>Male</div>
-          <div>Kids</div>
-          <div>All Products</div>
-        </div>
-        {/* Search bar */}
-        <div className="border-2 border-gray-300 h-6 px-2 rounded-md flex items-center">
-          <svg
-            stroke="currentColor"
-            fill="currentColor"
-            stroke-width="0"
-            version="1.1"
-            id="search"
-            x="0px"
-            y="0px"
-            viewBox="0 0 24 24"
-            height="1em"
-            width="1em"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g>
-              <path
-                d="M20.031,20.79c0.46,0.46,1.17-0.25,0.71-0.7l-3.75-3.76c1.27-1.41,2.04-3.27,2.04-5.31
-		c0-4.39-3.57-7.96-7.96-7.96s-7.96,3.57-7.96,7.96c0,4.39,3.57,7.96,7.96,7.96c1.98,0,3.81-0.73,5.21-1.94L20.031,20.79z
-		 M4.11,11.02c0-3.84,3.13-6.96,6.96-6.96c3.84,0,6.96,3.12,6.96,6.96c0,3.84-3.12,6.96-6.96,6.96C7.24,17.98,4.11,14.86,4.11,11.02
-		z"
-              ></path>
-            </g>
-          </svg>
-          <input
-            className=" bg-white text-sm rounded-md w-80 px-2"
-            type="search"
-            name="search"
-            placeholder="What you're looking for"
-          />
-        </div>
-        {/* Cart button */}
-        <button className="bg-gray-200 rounded-full p-3 flex relative">
-          <svg
-            stroke="currentColor"
-            fill="none"
-            stroke-width="0"
-            viewBox="0 0 24 24"
-            height="22"
-            width="22"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
+      {/* TopBar */}
+      {/* Mobile popup open and close svgs */}
+      <div
+        onClick={handleOpen}
+        className="fixed right-28 top-16 z-10 lg:hidden"
+      >
+        {open ? (
+          <>
+            <svg
+              stroke="currentColor"
               fill="currentColor"
-            ></path>
-            <path
-              d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              color="black"
+              font-size="27"
+              //   style="color:black"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M3 4h18v2H3V4zm6 7h12v2H9v-2zm-6 7h18v2H3v-2z"></path>
+              </g>
+            </svg>
+          </>
+        ) : (
+          <>
+            <svg
+              stroke="currentColor"
               fill="currentColor"
-            ></path>
-            <path
-              d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
-              fill="currentColor"
-            ></path>
-          </svg>
-          <span className="bg-red-600 text-white rounded-full h-4 w-4 text-sm absolute top-0 right-1">
-            0
-          </span>
-        </button>
-      </nav>
+              stroke-width="0"
+              viewBox="0 0 24 24"
+              color="black"
+              font-size="27"
+              //   class="close_icon"
+              height="1em"
+              width="1em"
+              xmlns="http://www.w3.org/2000/svg"
+              //   style="color: black;"
+            >
+              <g>
+                <path fill="none" d="M0 0h24v24H0z"></path>
+                <path d="M12 10.586l4.95-4.95 1.414 1.414-4.95 4.95 4.95 4.95-1.414 1.414-4.95-4.95-4.95 4.95-1.414-1.414 4.95-4.95-4.95-4.95L7.05 5.636z"></path>
+              </g>
+            </svg>
+          </>
+        )}
+      </div>
+      {/* Dine market logo */}
+      <div className="fixed left-28 top-16 z-10 lg:hidden">
+        <Image
+          src="/Logo.webp"
+          height={25}
+          width={140}
+          alt="Dine market logo"
+        />
+      </div>
+      {!open && (
+        <div className="lg:hidden flex justify-center items-center h-screen w-full bg-white">
+          {/* List of links */}
+          <div className="flex flex-col items-center gap-4 fixed">
+            {/* Cart button */}
+            <button className="bg-gray-200 rounded-full p-3 relative">
+              <svg
+                stroke="currentColor"
+                fill="none"
+                stroke-width="0"
+                viewBox="0 0 24 24"
+                height="22"
+                width="22"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M5.79166 2H1V4H4.2184L6.9872 16.6776H7V17H20V16.7519L22.1932 7.09095L22.5308 6H6.6552L6.08485 3.38852L5.79166 2ZM19.9869 8H7.092L8.62081 15H18.3978L19.9869 8Z"
+                  fill="currentColor"
+                ></path>
+                <path
+                  d="M10 22C11.1046 22 12 21.1046 12 20C12 18.8954 11.1046 18 10 18C8.89543 18 8 18.8954 8 20C8 21.1046 8.89543 22 10 22Z"
+                  fill="currentColor"
+                ></path>
+                <path
+                  d="M19 20C19 21.1046 18.1046 22 17 22C15.8954 22 15 21.1046 15 20C15 18.8954 15.8954 18 17 18C18.1046 18 19 18.8954 19 20Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              <span className="bg-red-600 text-white rounded-full h-4 w-4 text-sm absolute top-0 right-1">
+                0
+              </span>
+            </button>
+            <div>Female</div>
+            <div>Male</div>
+            <div>Kids</div>
+            <div>All Products</div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
