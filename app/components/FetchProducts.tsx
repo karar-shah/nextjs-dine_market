3d58574dde9f8f4c48fd68bd4f642a410b12028c
karar-shah <karar.shah2015@gmail.com>
2023-06-02 20:17:58 +0500

    fetching products - minor error

---
+++ app/components/FetchProducts.tsx
@@ -0,0 +1,28 @@
+import { client } from "../lib/sanityClient";
+
+export const getProductData = async () => {
+  const res = await client.fetch(
+    `*[_type=="product" && _id=="b704ea69-06bc-4d1b-b303-146f5a8378ab"]`
+  );
+  return res;
+};
+
+interface IProducts {
+  title: string;
+  price: string;
+}
+
+export default async function FetchProducts() {
+  const data: IProducts[] = await getProductData();
+  console.log(data);
+  return (
+    <>
+      {data.map((items) => (
+        <div key={items.title}>
+          <div>{items.title}</div>
+          <div>{items.price}</div>
+        </div>
+      ))}
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,38 @@
 import Features from "./components/Features";
+import FetchProducts from "./components/FetchProducts";
 import Footer from "./components/Footer";
 import Hero2 from "./components/Hero2";
 import Nav from "./components/Nav";
 import NewsLetter from "./components/NewsLetter";
 import Products from "./components/Products";
 import Promotions from "./components/Promotions";
+import { client } from "./lib/sanityClient";
 
-export default function Home() {
+export const getProductData = async () => {
+  const res = await client.fetch(
+    `*[_type=="product" && _id=="b704ea69-06bc-4d1b-b303-146f5a8378ab"]`
+  );
+  return res;
+};
+
+interface IProducts {
+  title: string;
+  price: string;
+}
+
+export default async function Home() {
+  const data: IProducts[] = await getProductData();
+  console.log(data);
   return (
     <div>
       <Nav />
+      {data.map((items) => (
+        <div key={items.title}>
+          <div>{items.title}</div>
+          <div>{items.price}</div>
+        </div>
+      ))}
+      {/* <FetchProducts /> */}
       <Hero2 />
       <Promotions />
       <Products />
