17bb8947fb8fd426daae971946068d89bb002872
Sajid Munawar <iamsajiddev@gmail.com>
2023-06-06 08:28:44 +0500

    initial design for mobile

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -4,17 +4,17 @@ import React from "react";
 export default function page() {
   return (
     <>
-      <div className="px-32">
+      <div className="px-8 lg:px-32">
         <h1 className="mb-8 text-3xl font-bold">Shopping Cart</h1>
         {/* Cart Box */}
-        <div className="flex">
+        <div className="flex flex-col gap-4">
           {/* product image and summary container */}
-          <div className="flex">
+          <div className="">
             {/* image */}
             <div>
               <Image
                 src={"/product1.png"}
-                height={250}
+                height={300}
                 width={250}
                 alt="product"
                 className="rounded-md"
@@ -22,9 +22,11 @@ export default function page() {
             </div>
             {/* product details */}
             <div>
-              <div className="flex justify-between">
-                Flex Sweatshirt{" "}
-                <span>
+              <div className="flex pt-8">
+                <div className="text-2xl font-light">
+                  Brushed Raglan Sweatshirt
+                </div>
+                <div className="ml-auto flex">
                   <svg
                     stroke="currentColor"
                     fill="none"
@@ -41,7 +43,46 @@ export default function page() {
                       d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                     ></path>
                   </svg>
-                </span>
+                </div>
+              </div>
+              <div>Dress</div>
+              <div>Delivery Estimateion</div>
+              <div>5 Working days</div>
+              <div className="flex">
+                <div className="text-2xl font-light">$195</div>
+                <div className="ml-auto flex items-center gap-4">
+                  {/* Minus */}
+                  <svg
+                    stroke="currentColor"
+                    fill="currentColor"
+                    stroke-width="0"
+                    viewBox="0 0 1024 1024"
+                    height="1em"
+                    width="1em"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M872 474H152c-4.4 0-8 3.6-8 8v60c0 4.4 3.6 8 8 8h720c4.4 0 8-3.6 8-8v-60c0-4.4-3.6-8-8-8z"></path>
+                  </svg>
+                  <span>1</span>
+                  {/* Plus */}
+                  <div className="rounded-full border border-gray-900 p-1 text-black">
+                    <svg
+                      stroke="currentColor"
+                      fill="currentColor"
+                      stroke-width="0"
+                      t="1551322312294"
+                      viewBox="0 0 1024 1024"
+                      version="1.1"
+                      height="1em"
+                      width="1em"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <defs></defs>
+                      <path d="M474 152m8 0l60 0q8 0 8 8l0 704q0 8-8 8l-60 0q-8 0-8-8l0-704q0-8 8-8Z"></path>
+                      <path d="M168 474m8 0l672 0q8 0 8 8l0 60q0 8-8 8l-672 0q-8 0-8-8l0-60q0-8 8-8Z"></path>
+                    </svg>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
