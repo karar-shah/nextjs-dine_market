f128a029921c5b7700e2483efac2f0a667759e6f
karar-shah <karar.shah2015@gmail.com>
2023-06-09 21:19:34 +0500

    fixed some ui

--- app/all-products/page.tsx
+++ app/all-products/page.tsx
@@ -14,16 +14,23 @@ export default async function Page() {
   const data: IProductsDetail[] = await getAllProductsData();
   return (
     <>
-      <div className="grid auto-cols-max grid-cols-1 gap-6 px-28 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid auto-cols-max grid-cols-1 gap-6 px-8 py-16 md:grid-cols-2 md:p-16 lg:grid-cols-3 lg:px-32 lg:py-16 xl:grid-cols-4">
         {data.map((item: IProductsDetail) => (
-          <Link key={item.title} href={`/products/${item.title}`}>
-            <Image
-              src={urlForImage(item.image).url()}
-              alt={item.title}
-              height={330}
-              width={280}
-              quality={100}
-            />
+          <Link
+            key={item.title}
+            href={`/products/${item.title}`}
+            className="ml-5 md:ml-0"
+          >
+            <div className="relative h-[300px] w-[280px] md:h-[270px] md:w-[250px]">
+              <Image
+                src={urlForImage(item.image).url()}
+                alt={item.title}
+                fill={true}
+                // height={330}
+                // width={280}
+                quality={100}
+              />
+            </div>
             <div className="py-4 text-xl font-semibold">{item.title}</div>
             <div className="pb-4 text-xl font-bold">${item.price}</div>
           </Link>
