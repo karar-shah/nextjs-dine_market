850ac0176fc3dabfacac8ec20477335dea0278d7
karar-shah <karar.shah2015@gmail.com>
2023-06-13 19:01:59 +0500

    fixed async commit and hook

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,7 +3,6 @@ import AddToCart from "./AddToCart";
 export default function CartPage() {
   return (
     <div>
-      {/* @ts-expect-error Server Component */}
       <AddToCart />;
     </div>
   );
